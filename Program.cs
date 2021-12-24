8771b3a019aec39a5b0781f7995d63351b17aa2d
Aigio Liu <lysoft.pw@foxmail.com>
2021-12-24 23:11:40 +0800

    v1.0

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+var scriptType = Type.GetTypeFromCLSID(Guid.Parse("0E59F1D5-1FBE-11D0-8FF2-00A0D10038BC"));
+dynamic obj = Activator.CreateInstance(scriptType, false);
+obj.Language = "vbscript";
+const string content = "CreateObject(\"InternetExplorer.Application\").Visible=true";
+obj.ExecuteStatement(content);
